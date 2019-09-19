d0917c017b07524e27d5a481dce65192c7d2c224
Urooj Abbas <urooj.abbas@gmail.com>
2019-09-19 07:54:10 +0500

    bug fix
    bug fix

--- src/main/java/com/vroom/dataservice/controller/InvalidRequestController.java
+++ src/main/java/com/vroom/dataservice/controller/InvalidRequestController.java
@@ -1,16 +1,18 @@
 package com.vroom.dataservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class InvalidRequestController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public Object redirectInvalidRequest() {
-        return "redirect:/index.html";
+    private static final Logger logger = LoggerFactory.getLogger(InvalidRequestController.class);
+    @RequestMapping (value = "/invalid", method = {RequestMethod.GET, RequestMethod.POST})
+    public ResponseEntity<String> defaultPath() {
+        logger.info("Unmapped request handling!");
+        return new ResponseEntity<String>("Unmapped request", HttpStatus.OK);
     }
 }
